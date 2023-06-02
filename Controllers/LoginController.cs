80b406165267e3cab5d803076acf03d51731c26f
Your Name <you@example.com>
2023-06-02 11:47:01 +0700

    Update Logincontroller, edit Index Home (view), and Login View Page

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,7 +4,7 @@ namespace LogWorkWebSite.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
